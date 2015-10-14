0ad1e7071832276547e785a8acad497c89f6938d
Ubuntu <ubuntu@ip-172-31-31-79.ap-southeast-1.compute.internal>
2015-10-14 04:46:45 +0000

    datejoined and last login display field to user admin added

--- apps/restaurants/admin.py
+++ apps/restaurants/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from nested_inline.admin import NestedStackedInline, NestedModelAdmin, NestedTabularInline
 from .models import *
 
+admin.site.unregister(User)
+class CustomUserAdmin(UserAdmin):
+	list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff','date_joined', 'last_login')
+admin.site.register(User, CustomUserAdmin) 
 # Register your models here.
 
 admin.site.register(Cuisine)
