b5294d804f7357f4fd1ce7fe976fd0e3a7db9c2d
Varun Bansal <varun13168@iiitd.ac.in>
2015-10-17 22:01:58 +0530

    session storage added, cart won't refresh on page reload

--- staticfiles/js/cart.js
+++ staticfiles/js/cart.js
@@ -1,143 +1,93 @@
+mylistPriceType = [];
+if(sessionStorage.getItem('mylistPriceType')!==null){
+	var mylistPriceTypeString = sessionStorage.getItem('mylistPriceType');
+	mylistPriceTypeString = mylistPriceTypeString.split(',');
+	for(i = 0; i<mylistPriceTypeString.length; i++){
+		mylistPriceType.push(mylistPriceTypeString[i]);
+	}
+}
+// var itemQty;
+function addItem (qtyID, dishName, vOn, priceType, tdClass, price, priceTypeId) {
+	itemQty = document.getElementsByClassName(String(qtyID))[0].innerHTML;
+	document.getElementsByClassName(String(qtyID))[0].innerHTML = String(1 + Number(itemQty));
+	var table = document.getElementById("mycart");
+	var index;
+	var n = mylistPriceType.length;
+	var a = document.getElementsByClassName("total")[0].innerHTML;
+	var tot = Number(a) + Number(price);
+	document.getElementsByClassName("total")[0].innerHTML = tot;
+	document.getElementsByClassName("total")[1].innerHTML = tot;
+	document.getElementsByClassName("finalpay")[0].innerHTML = tot;
+	document.getElementsByClassName("finalpay")[1].innerHTML = tot;
+	// document.getElementsByClassName("servTax")[0].innerHTML = tot;
 
-// function addBill(itemPrice){
+	for(index = 0; index<n; index++){
+		// console.log(mylistPriceType[index] == String(priceTypeId));
+		if(mylistPriceType[index] == String(priceTypeId)){
+			var cell = document.getElementById(String(priceTypeId));
+			// var cell = row.insertCell(3);
+			cell.innerHTML = document.getElementById(String(tdClass)).innerHTML;	
+			return;
+		}
+	}
+	mylistPriceType.push(String(priceTypeId));
+	var row = table.insertRow(0);
+	var cell2 = row.insertCell(0);
+	var cell1 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	cell3.id = priceTypeId;
+	cell1.innerHTML = String(dishName);
+	var sym = "";
+	if(vOn=='V')
+		sym = String("<span style='color:green;padding:0 .5em'>&#9679;</span>");
+	else if(vOn=='N')
+		sym = String("<span style='color:red;padding:0 .5em'>&#9679;</span>");
+	cell2.innerHTML = sym + String(priceType);
+	cell3.innerHTML = document.getElementById(String(tdClass)).innerHTML;
+	cell4.innerHTML = "&#8377;" + String(price);
+	console.log(mylistPriceType);
+	sessionStorage.setItem('mylistPriceType', mylistPriceType);
+	console.log( "yo yo "+sessionStorage.getItem('mylistPriceType'));
 
-// }
-// function decBill(itemPrice){
-
-// }
-// function addItem(myId, item, itemDish, itemQty, itemPrice, itemId, qtyId){
-// 	var current = document.getElementById(Number(myId)).value;
-// 	current = Number(current);
-// 	current+=1;
-// 	document.getElementById(Number(myId)).value = String(current);
-// 	// addBill(itemPrice);
-
-// 	if (document.getElementById(String(itemId))){
-// 		var node = document.getElementById(String(itemId));
-// 		var item = [itemDish, itemQty, itemPrice, current];
-// 	var itemDetails = document.createTextNode("" + item[0] + " " + item[1] + " "  + item[2] + "-" + item[3]);
-// 		node.removeChild(node.firstChild);
-// 		node.appendChild(itemDetails);
-// 		document.getElementById("mycart").appendChild(node);
-// 	}
-// 	else{
-// 		var node = document.createElement("LI");
-// 		node.id = itemId;
-// 		itemN = '1';
-// 		var item = [itemDish, itemQty, itemPrice, current];
-// 	var itemDetails = document.createTextNode("" + item[0] + " " + item[1] + " "  + item[2] + "-" + item[3]);
-// 		node.appendChild(itemDetails);
-// 		document.getElementById("mycart").appendChild(node);
-// 	}
-// }
-
-// function deleteItem(myId, item, itemDish, itemQty, itemPrice, itemId, qtyId){
-// 	var current = document.getElementById(Number(myId)).value;
-// 	current = Number(current);
-// 	if(current > 0){
-// 		current-=1;
-// 		// decBill(itemPrice);
-// 		document.getElementById(Number(myId)).value = String(current);
-
-// 		if (document.getElementById(String(itemId))){
-// 			if(current>0){
-// 				var node = document.getElementById(String(itemId));
-// 				var item = [itemDish, itemQty, itemPrice, current];
-// 				node.removeChild(node.firstChild);		
-// 				var itemDetails = document.createTextNode("" + item[0] + " " + item[1] + " "  + item[2] + "-" + item[3]);
-// 				node.appendChild(itemDetails);
-// 				document.getElementById("mycart").appendChild(node);
-// 			}
-// 			else{
-// 				document.getElementById(String(itemId)).remove();
-// 			}
-// 		}
-// 	}
-// }
-
-	mylistPriceType = [];
-	// var itemQty;
-	function addItem (qtyID, dishName, vOn, priceType, tdClass, price, priceTypeId) {
-		itemQty = document.getElementsByClassName(String(qtyID))[0].innerHTML;
-		document.getElementsByClassName(String(qtyID))[0].innerHTML = String(1 + Number(itemQty));
-		var table = document.getElementById("mycart");
-		var index;
-		var n = mylistPriceType.length;
+}
+function delItem(qtyID, dishName, vOn, priceType, tdClass, price, priceTypeId){
+	itemQty = document.getElementsByClassName(String(qtyID))[0].innerHTML;
+	if(Number(itemQty) > 0){
+		document.getElementsByClassName(String(qtyID))[0].innerHTML = String(Number(itemQty)-1);	
 
 		var a = document.getElementsByClassName("total")[0].innerHTML;
-		var tot = Number(a) + Number(price);
+		var tot = String(Number(a) - Number(price));
 		document.getElementsByClassName("total")[0].innerHTML = tot;
 		document.getElementsByClassName("total")[1].innerHTML = tot;
 		document.getElementsByClassName("finalpay")[0].innerHTML = tot;
 		document.getElementsByClassName("finalpay")[1].innerHTML = tot;
-		// document.getElementsByClassName("servTax")[0].innerHTML = tot;
-
-		for(index = 0; index<n; index++){
-			// console.log(mylistPriceType[index] == String(priceTypeId));
-			if(mylistPriceType[index] == String(priceTypeId)){
-				var cell = document.getElementById(String(priceTypeId));
-				// var cell = row.insertCell(3);
-				cell.innerHTML = document.getElementById(String(tdClass)).innerHTML;	
-				return;
-			}
-		}
-		mylistPriceType.push(String(priceTypeId));
-		var row = table.insertRow(0);
-		var cell2 = row.insertCell(0);
-		var cell1 = row.insertCell(1);
-		var cell3 = row.insertCell(2);
-		var cell4 = row.insertCell(3);
-		cell3.id = priceTypeId;
-		cell1.innerHTML = String(dishName);
-		var sym = "";
-		if(vOn=='V')
-			sym = String("<span style='color:green;padding:0 .5em'>&#9679;</span>");
-		else if(vOn=='N')
-			sym = String("<span style='color:red;padding:0 .5em'>&#9679;</span>");
-		cell2.innerHTML = sym + String(priceType);
-		cell3.innerHTML = document.getElementById(String(tdClass)).innerHTML;
-		cell4.innerHTML = "&#8377;" + String(price);
-
-
-
-
 	}
-	function delItem(qtyID, dishName, vOn, priceType, tdClass, price, priceTypeId){
-		itemQty = document.getElementsByClassName(String(qtyID))[0].innerHTML;
-		if(Number(itemQty) > 0){
-			document.getElementsByClassName(String(qtyID))[0].innerHTML = String(Number(itemQty)-1);	
+	var index;
+	var n = mylistPriceType.length;
 
-			var a = document.getElementsByClassName("total")[0].innerHTML;
-			var tot = String(Number(a) - Number(price));
-			document.getElementsByClassName("total")[0].innerHTML = tot;
-			document.getElementsByClassName("total")[1].innerHTML = tot;
-			document.getElementsByClassName("finalpay")[0].innerHTML = tot;
-			document.getElementsByClassName("finalpay")[1].innerHTML = tot;
+	for(index = 0; index<n; index++){
+		if(mylistPriceType[index] == String(priceTypeId)){
+			var cell = document.getElementById(String(priceTypeId));
+			cell.innerHTML = document.getElementById(String(tdClass)).innerHTML;	
+			return;
 		}
-		var index;
-		var n = mylistPriceType.length;
-
-		for(index = 0; index<n; index++){
-			if(mylistPriceType[index] == String(priceTypeId)){
-				var cell = document.getElementById(String(priceTypeId));
-				cell.innerHTML = document.getElementById(String(tdClass)).innerHTML;	
-				return;
-			}
-		}
-
 	}
+	sessionStorage.setItem('mylistPriceType', mylistPriceType);
 
-	function submitBill(){
-		var tbl = document.getElementById("mycart");
-		console.log(tbl.innerText);
-		var txt = tbl.innerText;
-		txt = txt.split(txt[0]);
-		var mybillstring = "";
-		for(i = 0; i<txt.length; i++){
-			mybillstring +=txt[i];
-			mybillstring +=";";
-		}
-		document.getElementById("mycartValue").setAttribute('value', mybillstring);
-		$("#billForm").submit();
+}
 
+function submitBill(){
+	var tbl = document.getElementById("mycart");
+	console.log(tbl.innerText);
+	var txt = tbl.innerText;
+	txt = txt.split(txt[0]);
+	var mybillstring = "";
+	for(i = 0; i<txt.length; i++){
+		mybillstring +=txt[i];
+		mybillstring +=";";
 	}
+	document.getElementById("mycartValue").setAttribute('value', mybillstring);
+	$("#billForm").submit();
+
+}
